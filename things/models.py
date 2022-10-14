a5017a1335481fceeafee23d5e348e388042ab48
Charlotte Leong <charlotte.leong@kcl.ac.uk>
2022-10-14 13:24:57 +0100

    create basic model with name description and quantity attributes

--- things/models.py
+++ things/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+#from django.db.models import Model
 
-# Create your models here.
+class User(AbstractUser):
+    name = models.TextField()
+    description = models.TextField()
+    quantity = models.IntegerField()
