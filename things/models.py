f9d9602fd2ddbc97d83052dd58dcc12fefd8d8ca
Charlotte Leong <charlotte.leong@kcl.ac.uk>
2022-10-14 14:37:45 +0100

    set constraints on Thing attributes

--- things/models.py
+++ things/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from enum import unique
+from django.core.exceptions import ValidationError
+
+def validate_number(value):
+    if value < 0 or value >100:
+        raise ValisationError("number must be between 0 and 100 (inclusive)")
 
 class Thing(models.Model):
-    name = models.TextField()
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.CharField(max_length = 30, unique = True)
+    description = models.CharField(max_length = 120, unique = False)
+    quantity = models.IntegerField(validators = [validate_number])
